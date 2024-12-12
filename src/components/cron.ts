945bd186031d65f46d7a0566c93fdcbb4ba2c32b
João Victor Maia <maia.joaovic@gmail.com>
2024-12-12 16:49:05 -0300

    feat: função do cron iniciada

--- src/components/cron.ts
+++ src/components/cron.ts
@@ -1,1 +1,21 @@
-console.log('cron');
+import parser from 'cron-parser'
+import $ from 'jquery'
+
+export interface TempoJob {
+    minuto: string,
+    hora: string,
+    diaMes: string,
+    diaSemana: string,
+    mes: string
+}
+
+export default class CronSeletor {
+    static exibirProximoJob(tempoJob : TempoJob) {
+        let proximoJob = parser.parseExpression(`${tempoJob.minuto} ${tempoJob.hora} ${tempoJob.diaMes} ${tempoJob.mes} ${tempoJob.diaSemana}`).next();
+
+        let dataProximoJob = `${proximoJob.getDate()}/${proximoJob.getMonth()}/${proximoJob.getFullYear()} às ${proximoJob.getHours()}:${proximoJob.getMinutes()}:${proximoJob.getSeconds()}`
+
+        $(".texto-resultado").html(dataProximoJob.toString());
+        
+    }
+}
--- src/pages/home/index.ts
+++ src/pages/home/index.ts
@@ -1,1 +1,31 @@
-console.log('home page');
+import $ from 'jquery'
+import CronSeletor, { TempoJob } from 'components/cron'
+
+export function usarCronTeste() {
+
+}
+
+function tratarEntradaInput(event) {
+    let regex = /^[0-9,-/*]$/;
+
+    if(!regex.test(event.key) && event.key != "Backspace") 
+        event.preventDefault();
+}
+
+$(".formulario-container").on("input", () => {
+    let informacaoFormulario = Object.fromEntries(new FormData(document.querySelector(".formulario-container")));
+
+    let informacaoJob: TempoJob = {
+        minuto: informacaoFormulario["minuto"].toString() || "*",
+        hora: informacaoFormulario["hora"].toString() || "*",
+        diaMes: informacaoFormulario["diaMes"].toString() || "*",
+        mes: informacaoFormulario["mes"].toString() || "*",
+        diaSemana: informacaoFormulario["diaSemana"].toString() || "*",
+    }
+
+    CronSeletor.exibirProximoJob(informacaoJob);
+})
+
+$(".formulario-input").on("keydown", (event) => {
+    tratarEntradaInput(event)
+})
